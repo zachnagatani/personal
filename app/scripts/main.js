a2d584f785cd74f0f872cc5df61b72e19f9544dc
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-10 19:57:59 -0700

    Feat: Expandable Portfolio Items

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,32 @@
 $(function() {
     'use strict';
 
-    // Set the height of the portfolio on load
-    $('#portfolio').height(window.innerHeight);
+    (function() {
+        // This closure's element is the portfolio section
+        const element = $('#portfolio'),
+              clickHandler = event => {
+                  // The vanilla JS event target
+                  let target = event.target,
+                  // The jQuery event target
+                      $target = $(target);
+                  const BASECLASS = 'section--portfolio__item',
+                        SELECTEDCLASS = 'section--portfolio__item--selected',
+                        MARGINOFFSET = 20;
+
+                  // Check if the target we clicked is expanded. If so, shrink it
+                  // If not, shrink any other expanded item and then expand it.
+                  if (target.className.includes(SELECTEDCLASS)) {
+                      $target.removeClass(SELECTEDCLASS).height(200);
+                  } else {
+                      $('.' + SELECTEDCLASS).removeClass(SELECTEDCLASS).height(200);
+                      $target.addClass(SELECTEDCLASS).height(window.innerHeight - MARGINOFFSET);
+                      $target.scrollView();
+                  }
+              };
+
+        // Set the height of the portfolio on load
+        element.height(window.innerHeight);
+
+        element.on('click', clickHandler);
+    })();
 });
